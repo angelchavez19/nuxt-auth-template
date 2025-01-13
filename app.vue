fb923297bdfd99c81de357d70e23ef788661711d
angelchavez19 <infoangelchavez@gmail.com>
2025-01-13 17:02:49 -0500

    Project setup

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import "~/assets/sass/globals.sass";
+</script>
+
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+useHead({ title: "Nuxt Auth Template" });
+</script>
+
+<template>Index Page</template>
+
+<style scoped lang="sass"></style>
